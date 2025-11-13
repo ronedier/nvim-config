f4663f708df7588ec528610a37532e4a86f0ffaf
Nikita Avgustanov <gh@nik-avg.org>
2025-11-13 13:46:26 +0400

    Added project.nvim usage

--- lua/custom/plugins/project.lua
+++ lua/custom/plugins/project.lua
@@ -1,17 +1,13 @@
 return {
   'DrKJeff16/project.nvim',
-  version = false, -- Get the latest release
   dependencies = { -- OPTIONAL
     'nvim-lua/plenary.nvim',
     'nvim-telescope/telescope.nvim',
     'ibhagwan/fzf-lua',
   },
-  ---@module 'project'
-
-  ---@type Project.Config.Options
-  opts = {},
   config = function()
     require('project').setup()
     require('telescope').load_extension 'projects'
   end,
+  vim.keymap.set('n', '<leader>sp', ':Telescope projects<CR>', { desc = '[S]earch [P]roject' }),
 }
