06cd1f36835bb25c69d25d08fe5aee0262a7e491
Nikita Avgustanov <gh@nik-avg.org>
2026-03-28 19:29:10 +0400

    changed keymaps for harpoon

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -32,33 +32,17 @@ return {
         :find()
     end
 
-    vim.keymap.set('n', '<C-h>', function()
-      toggle_telescope(harpoon:list())
-    end, { desc = 'Open harpoon window' })
+    vim.keymap.set('n', '<C-h>', function() toggle_telescope(harpoon:list()) end, { desc = 'Open harpoon window' })
 
-    vim.keymap.set('n', '<leader>ha', function()
-      harpoon:list():add()
-    end, { desc = '[H]arpoon [A]dd mark' })
+    vim.keymap.set('n', '<leader>ha', function() harpoon:list():add() end, { desc = '[H]arpoon [A]dd mark' })
 
-    vim.keymap.set('n', '<C-1>', function()
-      harpoon:list():select(1)
-    end)
-    vim.keymap.set('n', '<C-2>', function()
-      harpoon:list():select(2)
-    end)
-    vim.keymap.set('n', '<C-3>', function()
-      harpoon:list():select(3)
-    end)
-    vim.keymap.set('n', '<C-4>', function()
-      harpoon:list():select(4)
-    end)
+    vim.keymap.set('n', '<leader>h1', function() harpoon:list():select(1) end)
+    vim.keymap.set('n', '<leader>h2', function() harpoon:list():select(2) end)
+    vim.keymap.set('n', '<leader>h3', function() harpoon:list():select(3) end)
+    vim.keymap.set('n', '<leader>h4', function() harpoon:list():select(4) end)
 
     -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set('n', '<leader>hp', function()
-      harpoon:list():prev()
-    end, { desc = '[H]arpoon [P]rev item' })
-    vim.keymap.set('n', '<leader>hn', function()
-      harpoon:list():next()
-    end, { desc = '[H]arpoon [N]ext item' })
+    vim.keymap.set('n', '<leader>hp', function() harpoon:list():prev() end, { desc = '[H]arpoon [P]rev item' })
+    vim.keymap.set('n', '<leader>hn', function() harpoon:list():next() end, { desc = '[H]arpoon [N]ext item' })
   end,
 }
